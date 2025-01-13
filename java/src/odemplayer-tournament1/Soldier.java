ca33540d05de241ed24cfc7859c216d3e1323bc8
Ohad Offenbach <ohadoff2@gmail.com>
2025-01-13 21:28:32 +0200

    added 13/01 version player

---
+++ java/src/odemplayer-tournament1/Soldier.java
@@ -0,0 +1,98 @@
+package odemplayer;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapInfo;
+import battlecode.common.MapLocation;
+import battlecode.common.PaintType;
+import battlecode.common.RobotController;
+import battlecode.common.UnitType;
+
+class Soldier extends Globals {
+
+  enum SOLDIER_STATES{
+    scoutDirection, //scout a specific direction as assigned by the tower.
+    roam,           //walk randomly
+    notifyTower,    //found a ruin im not able to build. walk to notify tower
+    buildTower,     //found a ruin im able to build. build it.
+    attack,         //found a tower with enough friends to attack it.
+  }
+
+  static SOLDIER_STATES state = SOLDIER_STATES.roam;
+
+  // TODO: too long and clumsy - organize
+  public static void runSoldier(RobotController rc) throws GameActionException {
+
+    switch (state){
+
+        //region Roam
+        case roam:
+
+        //search for the closest ruin in range
+        MapInfo[] nearbyTiles = rc.senseNearbyMapInfos();
+        MapInfo currentRuin = null;
+        int currentDistance = 99999;
+
+        for (MapInfo tile : nearbyTiles) {
+          if (tile.hasRuin()) {
+            int distance = tile.getMapLocation().distanceSquaredTo(rc.getLocation());
+            if (distance < currentDistance) {
+              currentRuin = tile;
+              currentDistance = distance;
+            }
+          }
+        }
+
+
+        //if found ruin, go there.
+        if (currentRuin != null) {
+          MapLocation targetLocation = currentRuin.getMapLocation();
+          Direction dir = rc.getLocation().directionTo(targetLocation);
+          if (rc.canMove(dir)) {
+            rc.move(dir);
+          }
+
+          MapLocation checkMarked = targetLocation.subtract(dir);
+          if (rc.senseMapInfo(checkMarked).getMark() == PaintType.EMPTY
+                  && rc.canMarkTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLocation)) {
+            rc.markTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLocation);
+          }
+
+          for (MapInfo patternTile : rc.senseNearbyMapInfos(targetLocation, 8)) {
+            if (patternTile.getMark() != patternTile.getPaint() && patternTile.getMark() != PaintType.EMPTY) {
+              if (rc.canAttack(patternTile.getMapLocation())) {
+                if (patternTile.getMark() == PaintType.ALLY_SECONDARY) {
+                  rc.attack(patternTile.getMapLocation(), true);
+                } else {
+                  rc.attack(patternTile.getMapLocation(), false);
+                }
+              }
+            }
+          }
+
+          if (rc.canCompleteTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLocation)) {
+            rc.completeTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLocation);
+          }
+
+        }
+
+        MapLocation nextLoc = Utils.roamGracefullyf(rc);
+
+        if (rc.canAttack(nextLoc)) {
+          MapInfo nextLocInfo = rc.senseMapInfo(nextLoc);
+          if (!nextLocInfo.getPaint().isAlly()) {
+            rc.attack(nextLoc);
+          }
+        }
+        break;
+        //endregion
+
+
+    }
+
+
+
+
+  }
+
+}
---
+++ java/src/odemplayer-tournament1/Tower.java
@@ -0,0 +1,64 @@
+package odemplayer;
+
+import battlecode.common.*;
+
+public class Tower extends Globals {
+
+  // TODO: too long. improve
+  public static void runTower(RobotController rc) throws GameActionException {
+
+    GAME_PHASAE current_phase = GAME_PHASAE.early;
+
+    if (savingTurns == 0) {
+      isSaving = false;
+
+      //upgrade if able
+      if (rc.canUpgradeTower(rc.getLocation())) {
+        rc.upgradeTower(rc.getLocation());
+      }
+
+      Direction dir = directions[rng.nextInt(directions.length)];
+      MapLocation nextLocation = rc.getLocation().add(dir);
+      int robotType = rng.nextInt(4);
+
+      // build more soldiers
+      if (robotType == 0 || robotType == 1 && rc.canBuildRobot(UnitType.SOLDIER, nextLocation)) {
+        rc.buildRobot(UnitType.SOLDIER, nextLocation);
+        System.out.println("BUILT A SOLDIER");
+      }
+
+      else if (robotType == 2 && rc.canBuildRobot(UnitType.SPLASHER, nextLocation)) {
+        rc.buildRobot(UnitType.SPLASHER, nextLocation);
+        System.out.println("BUILT A SPLASHER");
+      }
+
+      else if (robotType == 3 && rc.canBuildRobot(UnitType.MOPPER, nextLocation)) {
+        rc.buildRobot(UnitType.MOPPER, nextLocation);
+        System.out.println("BUILT A MOPPER");
+      }
+    } else {
+      savingTurns--;
+      rc.setIndicatorString("Saving For " + savingTurns + "More Turns");
+    }
+
+    RobotInfo[] nearbyRobots = rc.senseNearbyRobots();
+    for (RobotInfo robot : nearbyRobots) {
+      if (rc.canAttack(robot.getLocation())) {
+        rc.attack(robot.getLocation());
+      }
+    }
+
+    // Read incoming messages
+    Message[] messages = rc.readMessages(-1);
+    for (Message m : messages) {
+      System.out.println("Tower received message: '#" + m.getSenderID() + " " + m.getBytes());
+
+      if (m.getBytes() == MessageType.SAVE_CHIPS.ordinal() && !isSaving) {
+        // TODO: Make more specific
+        savingTurns = 50;
+        isSaving = true;
+      }
+    }
+  }
+
+}
